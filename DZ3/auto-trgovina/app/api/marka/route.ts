488c4d3f54f982f9c3f4ee0bf166f208948f8a7a
Tereza <tereza.bilic@productive.io>
2024-05-26 22:03:02 +0200

    delete old project

--- DZ3/auto-trgovina/app/api/marka/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET(request: Request) {}
-
-export async function POST(request: Request) {}
-
-export async function PUT(request: Request) {}
-
-export async function DELETE(request: Request) {}
